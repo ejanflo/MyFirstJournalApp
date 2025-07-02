0d41a84167ddaaa5b0da5ddd877053a571d21809
El Jan Nerie Alburo <emalburo@anflocor.com>
2025-07-02 14:08:07 +0800

    Create MyFirstJournalApp with journal entry feature
    Removed template comment and added Program class.
    Implemented Main method to instantiate a JournalEntry object
    with ID, title, content, and timestamp. Displayed entry
    summary using GetSummary method in the console.

--- MyFirstJournalApp/Program.cs
+++ MyFirstJournalApp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyFirstJournalApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Add a new journal entry
+            JournalEntry entry = new JournalEntry
+            {
+                Id = 1,
+                Title = "My First Entry",
+                Content = "This is the content of my first journal entry.",
+                Timestamp = DateTime.Now
+            };
+
+            //Display GetSummary method
+            Console.WriteLine("Summary: " + entry.GetSummary());
+        }
+    }
+}
